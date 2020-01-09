401e72cf22693a74af8e72b152abfed70688a8e5
Jared Gain <jlgain7@gmail.com>
2020-01-08 20:58:54 -0600

    Updated Project 1 FullStackJS Files

--- JS/quote_generator.js
+++ JS/quote_generator.js
@@ -1,88 +1,101 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+Project 1 - A Random Quote Generator
 ******************************************/
 
 /*** 
- * `quotes` array with object literal
+ * `quotes` array of objects for data storage/quotes
 ***/
 let quotes = [
     {
         quote: "The way to get started is to quit talking and begin doing.",
         source: "Walt Disney",
-        citation: "Unknown",
+        citation: "",
+        tags: "Business",
         year: ""
     },
     {
         quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
         source: "Nelson Mandela",
-        citation: "Unknown",
+        citation: "",
+        tags: "Politics",
         year: ""
     },
     {
         quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
         source: "Steve Jobs",
         citation: "Speech",
+        tags: "Scholar",
         year: 2005
     },
     {
         quote: "If life were predictable it would cease to be life, and be without flavor.",
         source: "Eleanor Roosevelt",
-        citation: "Unknown",
+        citation: "",
+        tags: "Politics",
         year: ""
     },
     {
         quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-        source: "Oprh Winfrey",
-        citation: "Unknown",
+        source: "Oprah Winfrey",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
         source: "James Cameron",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "Life is what happens when you're busy making other plans.",
         source: "Allen Saunders",
         citation: "Publication",
+        tags: "",
         year: 1957
     },
     {
         quote: "Always remember that you are absolutely unique. Just like everyone else.",
         source: "Margaret Mead",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "The future belongs to those who believe in the beauty of their dreams.",
-        ource: "Elanor Roosevelt",
-        citation: "Unknown",
+        source: "Elanor Roosevelt",
+        citation: "",
+        tags: "Politics",
         year: ""
     },
     {
         quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
         source: "Benjamin Franklin",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "It is during our darkest moments that we must focus to see the light.",
         source: "Aristotle",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
         source: "Ralph Waldo Emerson",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
         source: "Robert Louis Stevenson",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
@@ -92,52 +105,75 @@ let quotes = [
             "and it becomes the bottle. You put it in a teapot, it becomes the teapot. Now, water can flow or it can crash. Be water, my friend.",
         source: "Bruce Lee",
         citation: "Publication",
+        tags: "",
         year: 2000
     },
     {
         quote: "You will face many defeats in life, but never let yourself be defeated.",
         source: "Maya Angelou",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "In the end, it's not the years in your life that count. It's the life in your years.",
         source: "Abraham Lincoln",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "Never let the fear of striking out keep you from playing the game.",
         source: "Babe Ruth",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
         source: "Thomas A. Edison",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.",
         source: "Dr. Seuss",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     },
     {
         quote: "The only impossible journey is the one you never begin.",
         source: "Tony Robbins",
         citation: "Speech",
+        tags: "",
         year: ""
     },
     {
         quote: "Life is a journey that must be traveled no matter how bad the roads and accommodations.",
         source: "Oliver Goldsmith",
-        citation: "Unknown",
+        citation: "",
+        tags: "",
         year: ""
     }
 ];
 
+// write to console to display all objects within the array
+console.log(quotes);
+
+/***
+ * `randomColor` function
+ ***/
+function getRandomColor()
+{
+    // generate random color for body background
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let rgbColor = "rgb(" + x + ", " + y + ", " + z + ")";
+    document.body.style.backgroundColor = rgbColor;
+}
 
 /***
  * `getRandomQuote` function
@@ -150,25 +186,35 @@ function getRandomQuote(quoteArray)
     return quoteArray[randomNum];
 }
 
+// write to console with random quote pulled from quotes array
+console.log(getRandomQuote(quotes));
+
 /***
  * `printQuote` function
 ***/
 function printQuote()
 {
+   // call randomColor function to create random color background
+   getRandomColor();
+
    // create a variable that calls the getRandomQuote() function
    let quotation = getRandomQuote(quotes);
 
    // create a variable that initiates your HTML string & add the two default quote and source property
-   let text = "";
+   let text = '';
    text += '<p class="quote">' + quotation.quote + '</p>';
-   text += '<p class="source">' + quotation.source + '</p>';
+   text += '<p class="source">' + quotation.source;
 
-   // conditional statement to add a citation and year if NOT null
-   if (quotes.citation !== null)
+   // conditional statement to add a citation, tag and year if NOT null
+   if (quotation.citation !== "")
    {
         text += '<span class="citation">' + quotation.citation + '</span>';
    }
-   if (quotes.year !== null)
+   if (quotation.tags !== "")
+   {
+        text += '<span class="tags">' + quotation.tags + '</span>';
+   }
+   if (quotation.year !== "")
    {
     text += '<span class="year">' + quotation.year + '</span>';
    }
@@ -176,13 +222,20 @@ function printQuote()
    // close HTML tag
    text += '</p>'; 
 
+   // write to console with new HTML string
+   console.log(text);
+
    // set innerHTML of .quote-box to complete HTML string
    document.getElementById('quote-box').innerHTML = text;
 }
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+ * `setInterval` function for set time
+ ***/
+setInterval(printQuote, 30000);
 
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW
+***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
